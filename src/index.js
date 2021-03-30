21fb864cfe114fe2140a1c6aeed0cd3fbaa4e03c
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-03-30 01:15:59 -0400

    Add home, menu, and contact pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import { pageLoad } from "./page_load.js";
 
+const body = document.querySelector("body");
+
 pageLoad.header();
-// pageLoad.home();
+pageLoad.nav();
+pageLoad.home();
+const nav = document.querySelector(".tabs");
+
+function removePreviousHTML() {
+  const eleToRemove = body.querySelector(".home") || body.querySelector(".menu") || body.querySelector(".contact");
+  eleToRemove.remove();
+  body.querySelector(".footer").remove();
+}
+
+nav.addEventListener("click", function (e) {
+  removePreviousHTML();
+
+  if (e.target.classList.contains("home-tab")) {
+    pageLoad.home();
+  } else if (e.target.classList.contains("menu-tab")) {
+    pageLoad.menu();
+    pageLoad.addMenuItem("./images/Krabby_Patty_2.png", "Krabby Patty", "2.00", "w/ sea cheese.");
+    pageLoad.addMenuItem("./images/Krabby_Patty_2.png", "Krabby Patty", "2.00", "w/ sea cheese.");
+    pageLoad.addMenuItem("./images/Krabby_Patty_2.png", "Krabby Patty", "2.00", "w/ sea cheese.");
+    pageLoad.addMenuItem("./images/Krabby_Patty_2.png", "Krabby Patty", "2.00", "w/ sea cheese.");
+    pageLoad.addMenuItem("./images/Krabby_Patty_2.png", "Krabby Patty", "2.00", "w/ sea cheese.");
+    pageLoad.addMenuItem("./images/Krabby_Patty_2.png", "Krabby Patty", "2.00", "w/ sea cheese.");
+  } else if (e.target.classList.contains("contact-tab")) {
+    pageLoad.contact();
+  }
+  pageLoad.footer();
+});
+
 pageLoad.footer();
--- src/page_load.js
+++ src/page_load.js
@@ -12,6 +12,26 @@ let pageLoad = (function () {
     header.append(headerTitle);
   }
 
+  function nav() {
+    const nav = document.createElement("nav");
+    nav.classList.add("tabs");
+
+    const button1 = document.createElement("button");
+    const button2 = document.createElement("button");
+    const button3 = document.createElement("button");
+
+    button1.classList.add("home-tab");
+    button2.classList.add("menu-tab");
+    button3.classList.add("contact-tab");
+
+    button1.textContent = "Home";
+    button2.textContent = "Menu";
+    button3.textContent = "Contact";
+
+    body.append(nav);
+    nav.append(button1, button2, button3);
+  }
+
   function home() {
     const home = document.createElement("section");
     home.classList.add("home");
@@ -25,13 +45,104 @@ let pageLoad = (function () {
 
     const chefText = document.createElement("div");
     chefText.classList.add("chef-text");
-    chefText.textContent = "Only the finest crimson wheat est. 1912!";
+    chefText.textContent =
+      "Only the finest burgers with real sea cheese! Come on down and see why The Krusty Krab is the most popular restaurant in Bikini Bottom!";
 
     body.append(home);
     home.append(chef);
     chef.append(chefImg);
     chef.append(chefText);
   }
+
+  function menu() {
+    const menu = document.createElement("section");
+    menu.classList.add("menu");
+
+    body.append(menu);
+  }
+
+  function contact() {
+    const contact = document.createElement("section");
+    contact.classList.add("contact");
+
+    const phone = document.createElement("div");
+    phone.classList.add("phone");
+
+    const phoneH1 = document.createElement("h1");
+    phoneH1.innerText = "By phone:";
+
+    const phoneNum = document.createElement("div");
+    phoneNum.innerText = "+00 00000 00000";
+
+    const mail = document.createElement("div");
+    mail.classList.add("mail");
+
+    const mailH1 = document.createElement("h1");
+    mailH1.innerText = "By mail:";
+
+    const mailAddress = document.createElement("div");
+    mailAddress.innerText = "Bikini Bottom, Pacific Ocean, 831 Bottom Feeder Lane";
+
+    const email = document.createElement("div");
+    email.classList.add("email");
+
+    const emailH1 = document.createElement("h1");
+    emailH1.innerText = "By email:";
+
+    const emailAddress = document.createElement("div");
+    emailAddress.innerText = "krustykrab@mail.com";
+
+    body.append(contact);
+    contact.append(phone);
+    phone.append(phoneH1);
+    phone.append(phoneNum);
+    contact.append(mail);
+    mail.append(mailH1);
+    mail.append(mailAddress);
+    contact.append(email);
+    email.append(emailH1);
+    email.append(emailAddress);
+  }
+
+  function addMenuItem(imagePath, nameStr, priceStr, descriptionStr) {
+    const menu = document.querySelector(".menu");
+
+    const menuItem = document.createElement("div");
+    menuItem.classList.add("menu-item");
+
+    const menuItemImg = document.createElement("img");
+    menuItemImg.classList.add("menu-item-img");
+    menuItemImg.src = imagePath;
+
+    const menuItemContainer = document.createElement("div");
+    menuItemContainer.classList.add("menu-item-container");
+
+    const menuItemHeader = document.createElement("div");
+    menuItemHeader.classList.add("menu-item-header");
+
+    const name = document.createElement("div");
+    name.classList.add("name");
+    name.textContent = nameStr;
+
+    const price = document.createElement("div");
+    price.classList.add("price");
+    price.textContent = priceStr;
+
+    const menuItemDescription = document.createElement("div");
+    menuItemDescription.classList.add("menu-item-description");
+    menuItemDescription.textContent = descriptionStr;
+
+    menu.append(menuItem);
+    menuItem.append(menuItemImg);
+    menuItem.append(menuItemContainer);
+    menuItemContainer.append(menuItemHeader);
+    menuItemHeader.append(name);
+    menuItemHeader.append(price);
+    menuItemContainer.append(menuItemDescription);
+    // menuItemDescription.append();
+  }
+
+  function contact() {}
 
   function footer() {
     const footer = document.createElement("section");
@@ -45,7 +156,7 @@ let pageLoad = (function () {
     footer.append(author);
   }
 
-  return { header, home, footer };
+    return { header, home, nav, menu, addMenuItem, contact, footer };
 })();
 
 export { pageLoad };
